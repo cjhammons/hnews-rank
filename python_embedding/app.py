36ef807497f1ff52b42b6c07a792b17cd021f0bb
Curtis Hammons <curtis@cjhammons.com>
2025-03-30 18:44:07 -0700

    updated debug logs

--- python_embedding/app.py
+++ python_embedding/app.py
@@ -88,4 +88,4 @@ load_tokenizer()
 # If running directly, not through gunicorn
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 6000))
-    app.run(host='0.0.0.0', port=port) 
+    app.run(host='0.0.0.0', port=port, debug=False) 
